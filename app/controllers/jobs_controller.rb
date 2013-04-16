690bd4f407a8827ae7662afd816ff9d851544217
Russell Schmidt <russell.schmidt@me.com>
2013-04-15 22:06:23 -0700

    Got nested delete to work

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,23 +12,6 @@ before_filter :authenticate_user!
   end
 
 
-  def edit
-    @resume = Resume.find(params[:resume_id])
-    @job = @resume.jobs.find(params[:id])
-  end
-
-  def update
-    @resume = Resume.find(params[:resume_id])
-    @job = @resume.jobs.find(params[:id])
-    if @job.update_attributes(params[:resume])
-      redirect_to resume_job_path(@resume, @job)
-    else
-      render action: :edit
-    end
-
-  end
-
-
   def new
     @resume = Resume.find(params[:resume_id])
     @job = @resume.jobs.build
@@ -44,4 +27,34 @@ before_filter :authenticate_user!
       render action: :new
     end
   end
+
+
+  def edit
+    @resume = Resume.find(params[:resume_id])
+    @job = @resume.jobs.find(params[:id])
+  end
+
+  def update
+    @resume = Resume.find(params[:resume_id])
+    @job = @resume.jobs.find(params[:id])
+
+    if @job.update_attributes(params[:job])
+      redirect_to resume_job_path(@resume, @job), notice: "Successful update"
+    else
+      render action: :edit
+    end
+
+  end
+
+  def destroy
+    @resume = Resume.find(params[:resume_id])
+    @job = @resume.jobs.find(params[:id])
+
+    if @job.destroy
+      redirect_to resume_jobs_url, notice: 'Job deleted'
+    else
+      redirect_to resume_jobs_path, notice: 'Job delete failed'
+    end
+  end
+
 end
--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,20 +1,32 @@
 class SchoolsController < ApplicationController
   before_filter :authenticate_user!
-
+  before_filter :resume_find, only: [:index, :show, :new, :create]
 
   def index
-      @resume = Resume.find(params[:resume_id])
-      @schools = @resume.schools.all
+    #@resume = Resume.find(params[:resume_id])
+    @schools = @resume.schools.all
   end
 
   def show
+    #@resume = Resume.find(params[:resume_id])
+    @school = @resume.schools.find(params[:id])
   end
 
 
   def new
+    #@resume = Resume.find(params[:resume_id])
+    @school = @resume.schools.build
   end
 
   def create
+    #@resume = Resume.find(params[:resume_id])
+    @school = @resume.schools.build(params[:school])
+
+    if @school.save
+      redirect_to resume_schools_path(@resume, @school)
+    else
+      render action: :new
+    end
   end
 
 
@@ -26,5 +38,10 @@ class SchoolsController < ApplicationController
 
 
   def destroy
+  end
+
+  private
+  def resume_find
+    @resume = Resume.find(params[:resume_id])
   end
 end
